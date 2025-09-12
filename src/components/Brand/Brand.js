40c4e8fa1d9cdfd8345f17866242a8db77b8d38f
Him1ko <hertlingj74@gmail.com>
2025-09-12 18:08:02 -0500

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,12 +10,8 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
-      <svg
-        width={127}
-        height={24}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 127.67 23.36"
+     {/* <h4>Hertling</h4> */}
+
       >
         <path
           fill="currentColor"
