2e399b935acc98b19ae3b21518fa3eae1f4c7998
Him1ko <hertlingj74@gmail.com>
2025-09-13 15:32:50 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -116,7 +116,7 @@ const IndexPage = () => {
       {/* Promotion */}
       <div className={styles.sustainableContainer}>
         <Hero
-          image={toOptimizedImage('/banner3.png')}
+          image={toOptimizedImage('/photo-1660394585016-508f949df960.jpeg')}
           title={'Materials & Process'}
           subtitle={
             'Every piece is crafted with care, using high-quality materials and thoughtful techniques. From start to finish, the process is designed to bring each unique vision to life.'
