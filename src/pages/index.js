cbbcb9818b8ac53a5d504f5ae6503b839dc3fca8
Him1ko <hertlingj74@gmail.com>
2025-10-04 17:34:11 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       {/* Hero Container */}
       <Hero
         maxWidth={'500px'}
-        image={'/photo-1660394585016-508f949df960.jpeg'}
+        image={'blurred gallery.webp'}
         title={'Art by Hertling'}
         subtitle={'Discover the Gallery'}
         ctaText={'shop now'}
